2d16af5863a32f408358a2a736b56dc300dc97c9
William Leung <gwilliamleung@gmail.com>
2022-08-16 16:52:23 -0400

    finished fetch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("HELLO")
+const movieList = document.getElementById("movieList")
+const movieSearch = document.getElementById("textSearch")
+
+document.getElementById("buttonSearch").addEventListener("click",function(){
+    movieList.textContent =''
+    fetch(`http://www.omdbapi.com/?s=${textSearch.value}&apikey=54959152`)
+        .then(res=>res.json())
+        .then(data=>{
+            // const { Title, RunTime, Genre, Plot, Poster, Ratings} = data;
+            console.log(data)
+            for(item in data.Search){
+                movieList.innerHTML += `
+                <div class="movieContent"></div>
+                <h3> ${data.Search[item].Title}
+                <h2>`
+            }
+        })
+})
